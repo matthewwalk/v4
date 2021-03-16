24e31f3e0104e8d7261d69af9baec3c05701e312
Matt Walker <matt.walker98@outlook.com>
2021-03-16 13:45:32 -0400

    Updates Global Styles
    * Replaced default colours with my own
    * TODO update colour references (i.e rename green to blue, etc.)

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,17 +7,20 @@ const GlobalStyle = createGlobalStyle`
   ${Fonts};
 
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
+    --dark-navy: #0B090B; //#020c1b;
+    --navy: #161216; //#0a192f;
+    --light-navy: #221C22; //#112240;
+    --lightest-navy: #2D252D; //#233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
-    --slate: #8892b0;
-    --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
+
+    --slate: #EEE0BF; //#8892b0;
+    --light-slate: #F2E7CF; //#a8b2d1;
+    --lightest-slate: #F6EFDF; //#ccd6f6;
+
     --white: #e6f1ff;
-    --green: #64ffda;
-    --green-tint: rgba(100, 255, 218, 0.1);
+    
+    --green: #01BAEF; //#64ffda;
+    --green-tint: #017FA2; //rgba(100, 255, 218, 0.1);
 
     --font-sans: 'Calibre', 'San Francisco', 'SF Pro Text', -apple-system, system-ui, sans-serif;
     --font-mono: 'SF Mono', 'Fira Code', 'Fira Mono', 'Roboto Mono', monospace;
